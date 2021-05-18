f69fca0357b5af16f2f4aa8478af127439b784ca
WilliamConyers <73149905+WilliamConyers@users.noreply.github.com>
2021-05-18 17:29:08 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,25 +1,10 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(plotly)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
     data <- read.csv("/Users/williamconyers/Desktop/Stat 310/Shiny/GapMinder/WorldBankData.csv")
-    
-    # observeEvent(input$region{
-    #     updateSelectInput(session, "region", choices = c("All Regions", unique(data$Region)))
-    # }, 
-    # once=TRUE
-    # )
 
     output$distPlot <- renderPlot({
         
